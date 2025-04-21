a020ac81144b23452466979e3c912e6be1a87708
FrozenLemonTee <1115306170@qq.com>
2025-04-21 12:15:24 +0800

    feat: Add split and  its implement

---
+++ include/utility.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include "vector"
+#include "string"
+
+std::vector<std::string> split(const std::string& str);
+
+#endif //UTILITY_H
---
+++ lib/utility.cpp
@@ -0,0 +1,13 @@
+#include <sstream>
+#include "utility.h"
+
+
+std::vector<std::string> split(const std::string &str) {
+    std::istringstream iss(str);
+    std::vector<std::string> res;
+    std::string cur;
+    while (iss >> cur){
+        res.emplace_back(cur);
+    }
+    return res;
+}
