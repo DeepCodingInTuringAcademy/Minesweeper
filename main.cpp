2324e9dc5c2bd6b0f12fc9d792db7b2ab4f06d56
Loveprayforme <145674747+Loveprayforme@users.noreply.github.com>
2025-05-18 17:15:06 +0800

    fix bug preliminarily

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    Game game(10,10,100);
-    game.startMenu();
-    while(!game.Over())
-    {
-        game.run();
-    }
+    Game game(10,10,51);
+
+    while (!game.Begin())game.startMenu();
+
+    while(!game.Over())game.run();
+
 }
