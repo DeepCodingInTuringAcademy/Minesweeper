513b442e12830c0fc5ad7e07a57597a3e2ae841d
FrozenLemonTee <1115306170@qq.com>
2025-05-28 13:23:13 +0800

    fix: Fix bug of path of assets

--- config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-#include "string"
-
-
-const std::string cwd = __FILE__;
-
-#endif //CONFIG_H
--- lib/soundPlay.cpp
+++ lib/soundPlay.cpp
@@ -1,8 +1,7 @@
 #include "soundPlay.h"
 #include "utility.h"
-#include "config.h"
 
 
-sf::Music main_music{getPath(cwd, {"assets", "Main.ogg"}).string()};
-sf::Music win_music{getPath(cwd, {"assets", "Win.wav"}).string()};
-sf::Music lose_music{getPath(cwd, {"assets", "Lose.ogg"}).string()};
+sf::Music main_music{getPath(getExecutableDir(), {"assets", "Main.ogg"}).string()};
+sf::Music win_music{getPath(getExecutableDir(), {"assets", "Win.wav"}).string()};
+sf::Music lose_music{getPath(getExecutableDir(), {"assets", "Lose.ogg"}).string()};
